9be1860d799489bd7a2d884e8634ade07347336a
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-10 22:55:33 +0300

    Data Types and Variables

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Inline comment. Setting up javascript.
+
+/* DATA TYPES AND VARIABLES.
+They are 7 and they include the following...
+Number,String,Boolean,Undefined,Null,Object,Symbol.
+*/ 
+
+/**Declaring Variables ft assigning
+
+ There are only 3 ways to declare a variable....
+ var,let,const.
+ */
+
+ var MyName = ("Noni Gathoni"); //Declaring Variables
+ var MyName;// Assigning Variables
+
+ 
