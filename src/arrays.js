573b29ec0cbe555dfa565c003801a325a6481929
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-11 19:42:27 +0300

    array test

--- src/arrays.js
+++ src/arrays.js
@@ -1,1 +1,11 @@
-// Arrays
+//ARRAYS
+/* Arrays are a collection of values which can be booleans,strings,numbers objects etc that are stored on a single variable.
+Use square brackets.
+
+ */
+
+// STRING ARRAYS.
+
+var fruits = ["mango , apples , bananas , cherries"];
+var result = fruits;
+console.log(result);
