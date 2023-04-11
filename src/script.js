9f5128e32ae10d61dca926991b84fafcfc03aefd
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-11 19:21:35 +0300

    wordBlanks

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@
 /* DATA TYPES AND VARIABLES.
 They are 7 and they include the following...
 Number,String,Boolean,Undefined,Null,Object,Symbol.
-*/ 
+*/
 
 /**Declaring Variables ft assigning
 
@@ -11,15 +11,15 @@ Number,String,Boolean,Undefined,Null,Object,Symbol.
  var,let,const.
  */
 
- var myName = ("xxxxxxxx"); //Declaring Variables
- var myName;// Assigning Variables
+var myName = ("xxxxxxxx"); //Declaring Variables
+var myName;// Assigning Variables
 
- 
+
 // INITIALIZING VARIABLES
 // Initializing variables is assigning them a value.
 var a = 9;
 var b = 8;
- 
+
 // Variables are case sensitive. Always use camelCase which basically means that the first case will always be small. eg
 
 var properCamelCase = ("This");
@@ -38,7 +38,7 @@ console.log(tot);
 
 
 
-/* Length of a string
+/* LENGTH OF A STRING
 */
 
 var country = "Jamaica";
@@ -49,7 +49,7 @@ var firstName = "Janet";
 var firstNameLength = firstName.length;
 console.log(firstNameLength);
 
-// First letter in a string 
+// FIRST LETTER IN A STRING. 
 // Always begin by setting which type of a variable you expect. Whether string or number etc
 // With a blank tab ie first line
 
@@ -65,13 +65,13 @@ console.log(secondLetterFirstName);
 
 
 var town = "Rongai";
-var lastLetterTown = town[town.length -1];
+var lastLetterTown = town[town.length - 1];
 console.log(lastLetterTown);
 
 
 
 
-// String Immutability.
+// STRING IMMUTABILITY..
 // strings value cannot be changed.
 // You cannot replace a letter on a string eg;
 
@@ -81,4 +81,13 @@ myCity[2] = "S"
 // This will outright cause an error since you cannot alter strings.
 
 
-// Functions
+// WORDBLANKS
+// They are used to practice string concatation with substitutes of variables in javascript.
+
+function wordBlanks(noun, Adjective, verb, adverb) {
+
+    var result = "";
+    result = "Kenya" +" "+ noun +" "+ Adjective +" " + verb +" "+ adverb + "."
+    return result;
+}
+console.log(wordBlanks("is", "very", "hot", "today"));
