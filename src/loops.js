c368e89e2d0f58c42360106ee8ecd96d32402b29
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-14 23:51:57 +0300

    While loops

--- src/loops.js
+++ src/loops.js
@@ -1,1 +1,17 @@
 // Lets get into loops!
+// WHILE LOOPS.
+
+// While loop is a control flow statement that gets excecuted as long as the specified condition is true.
+
+
+let x = 0;
+while (x < 10) {
+  console.log(x);
+  x += 2;
+}
+
+// it prints out 0,2,4,6,8.
+// 1. it prints out 0 because x (0) is less than 10.
+// 2. then it adds 2 to that zero x & creates (new X == 2).Because 2 is less than 10 it prints out (new X)which in this case is 2.
+// 3. Since the new X (2) is less than 10 still it adds 2 to make 4 which becomes a new x and it prints out 4 since its still less than 10.
+// This chain continues while adding on 2 until it breaks when the value of the x gets to 10.
