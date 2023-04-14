c7f5bb7332f707f2a34d35dea907962dd3f7be9d
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-14 17:40:21 +0300

    Bark prop added

--- src/objects.js
+++ src/objects.js
@@ -13,3 +13,23 @@ console.log(myDog.breed);
 
 // Accessing the properties using bracket notation.
 console.log(myDog["color"]);
+
+// Accessing Object properties with variables.
+// Create a new variable X with a new name (dogName) and then assign it one of the properties.
+// Create a new variable Y with another new name (dogsName) and then assign it to the object name while calling the previous variable X created.
+var dogName = "names";
+var dogsname = myDog[dogName];
+console.log(dogsname);
+
+// Prints out scoby liam since that our dog's name.
+
+// Updating Object Properties.
+
+myDog.names = "New Name Malik";
+console.log (myDog.names);
+
+// Adding new properties to my object.
+
+myDog.bark = "bark bark bark";
+console.log(myDog.bark);
+
