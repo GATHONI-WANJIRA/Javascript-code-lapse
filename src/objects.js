23253ae81e17fdebb186d08396d3ad33171b4b9d
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-14 17:02:59 +0300

    Sample Object

---
+++ src/objects.js
@@ -0,0 +1,11 @@
+// Objects are similar to arrays but instead of using indexe to access the other you use properties.
+// They are also defined by calibraces not square brackets like arrays.
+
+var myDog = {
+    // On the left we have properties of my dog while on the right we have the values.
+    names :"Scoby Liam",
+    age : 2,
+    color : "white",
+    breed : "german",
+};
+console.log(myDog.names);
