91e8e5ded7bf5eff9076b98f4a0cf6dd9355b61b
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-04-15 00:07:38 +0300

    For Loops

--- src/loops.js
+++ src/loops.js
@@ -15,3 +15,21 @@ while (x < 10) {
 // 2. then it adds 2 to that zero x & creates (new X == 2).Because 2 is less than 10 it prints out (new X)which in this case is 2.
 // 3. Since the new X (2) is less than 10 still it adds 2 to make 4 which becomes a new x and it prints out 4 since its still less than 10.
 // This chain continues while adding on 2 until it breaks when the value of the x gets to 10.
+
+
+
+// FOR LOOPS.
+// This is a control flow statement that allows you to execute code for a fixed number of times or a set sequence of values.
+
+// for (initialization; condition; increment) {
+//     // code block to be executed
+//   }
+  
+//   The "initialization" part is executed before the loop starts and initializes one or more variables in the loop.
+//  The "condition" part is evaluated before each iteration of the loop and it returns boolean values.
+// The "increment " part is only executed once the condition is true until its false.
+
+for (let i = 2;  (i <= 5); i++ ) {
+    console.log(i);
+}
+// Output is 2,3,4 ,5.
